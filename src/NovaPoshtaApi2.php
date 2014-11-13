77a988d785bd7b05f5a2e43b1f36ad36fb5a173b
lis-ua <nighter20@mail.ru>
2014-11-13 22:20:59 +0200

    Added public methods getCities - for getting list of cities by page, by string or by ref (city ID) getCity - getting single city by name of city and name of region(area) getArea - for getting area (from file) by string or by ref (area ID) Added protected methods findCityByRegion - get city from cities list by region findArea - get area from list of areas by string or ref (area ID)

--- src/NovaPoshtaApi2.php
+++ src/NovaPoshtaApi2.php
@@ -13,14 +13,18 @@ class NovaPoshtaApi2 {
 	 * 
 	 * @see https://my.novaposhta.ua/settings/index#apikeys
 	 */
-	 
-	private $key;
+	protected $key;
 	
 	/**
 	 * Format of returned data - array, json, xml
 	 */
-	private $format = 'array';
+	protected $format = 'array';
 	
+	/**
+	 * Areas (loaded from file, because there is no so function in NovaPoshta API 2.0)
+	 */
+	protected $areas;
+
 	/**
 	 * Default constructor
 	 * 
@@ -80,8 +84,11 @@ class NovaPoshtaApi2 {
 	 */
 	private function prepare($data) {
 		//Returns array
-		if ($this->format == 'array')
-			return json_decode($data, 1);
+		if ($this->format == 'array') {
+			return is_array($data)
+				? $data
+				: json_decode($data, 1);
+		}
 		// Returns json or xml document
 		return $data;
 	}
@@ -147,5 +154,129 @@ class NovaPoshtaApi2 {
 	 */
 	function documentsTracking($track) {
 		return $this->request('InternetDocument', 'documentsTracking', array('Documents' => array('item' => $track)));
+	}
+	
+	/**
+	 * Get cities of company NovaPoshta
+	 * 
+	 * @param int $page Num of page
+	 * @param string $findByString Find city by russian or ukrainian word
+	 * @param string $ref ID of city
+	 * @return mixed
+	 */
+	function getCities($page = 0, $findByString = '', $ref = '') {
+		return $this->request('Address', 'getCities', array(
+			'Page' => $page,
+			'FindByString' => $findByString,
+			'Ref' => $ref,
+		));
+	}
+	
+	/**
+	 * Find current area in list of areas
+	 * 
+	 * @param array $areas List of arias, getted from file
+	 * @param string $findByString Area name
+	 * @param string $ref Area Ref ID
+	 * @return array
+	 */
+	protected function findArea(array $areas, $findByString = '', $ref = '') {
+		$data = array();
+		if ( ! $findByString AND ! $ref)
+			return $data;
+		// Try to find current region
+		foreach ($areas as $key => $area) {
+			// Is current area found by string or by key
+			$found = $findByString 
+				? ((mb_stripos($area['Description'], $findByString) !== FALSE) 
+					OR (mb_stripos($area['DescriptionRu'], $findByString) !== FALSE)
+					OR (mb_stripos($area['Area'], $findByString) !== FALSE)
+					OR (mb_stripos($area['AreaRu'], $findByString) !== FALSE))
+				: ($key == $ref);
+			if ($found) {
+				$area['Ref'] = $key;
+				$data[] = $area;
+				break;
+			}
+		}
+		return $data;
+	}
+	
+	/**
+	 * Get area by name or by ID
+	 * 
+	 * @param string $findByString Find area by russian or ukrainian word
+	 * @param string $ref Get area by ID
+	 * @return array
+	 */
+	function getArea($findByString = '', $ref = '') {
+		// Load areas list from file
+		empty($this->areas) AND $this->areas = include dirname(__FILE__).'/NovaPoshtaApi2Areas.php';
+		$error = '';
+		$data = $this->findArea($this->areas, $findByString, $ref);
+		// Error
+		empty($data) AND $error = 'Area was not found';
+		// Return data in same format like NovaPoshta API
+		return $this->prepare(
+			array(
+				'success' => empty($error),
+				'data' => $data,
+				'errors' => (array) $error,
+				'warnings' => array(),
+				'info' => array(),
+		));
+	}
+	
+	/**
+	 * Find city from list by name of region
+	 * 
+	 * @param array $cities Array from query getCities to NovaPoshta 
+	 * @param string $areaName
+	 * @return array 
+	 */
+	protected function findCityByRegion($cities, $areaName) {
+		$data = array();
+		$areaRef = '';
+		// Get region id
+		$area = $this->getArea($areaName);
+		$area['success'] AND $areaRef = $area['data'][0]['Ref'];
+		if ($areaRef AND is_array($cities['data'])) {
+			foreach($cities['data'] as $city) {
+				if ($city['Area'] == $areaRef) {
+					$data[] = $city;
+				}
+			}
+		}
+		return $data;
+	}
+	
+	/**
+	 * Get city by name and region (if it needs)
+	 * 
+	 * @param string $cityName City's name
+	 * @param string $areaName Region's name
+	 * @return array City's data
+	 */
+	function getCity($cityName, $areaName = '') {
+		$error = '';
+		// Get cities by name
+		$cities = $this->getCities(0, $cityName);
+		if (is_array($cities['data'])) {
+			// If cities more then one, calculate current by area name
+			$data = (count($cities['data']) > 1) 
+				? $this->findCityByRegion($cities, $areaName)
+				: $cities['data'][0];
+		}
+		// Error
+		( ! $data) AND $error = 'City was not found';
+		// Return data in same format like NovaPoshta API
+		return $this->prepare(
+			array(
+				'success' => empty($error),
+				'data' => $data,
+				'errors' => (array) $error,
+				'warnings' => array(),
+				'info' => array(),
+		));
 	}
 }
