8678f6ef22a798048d768ea9083ba510ac4fcc37
Oleg <ezoterik.h@gmail.com>
2020-09-12 03:54:04 +0300

    Убрано безпричинное бросение исключения
    Ошибка была сделана здесь #36

--- src/Delivery/NovaPoshtaApi2.php
+++ src/Delivery/NovaPoshtaApi2.php
@@ -192,7 +192,7 @@ class NovaPoshtaApi2
                 ? $data
                 : json_decode($data, true);
             // If error exists, throw Exception
-            if ($this->throwErrors and array_key_exists('errors', $result)) {
+            if ($this->throwErrors and array_key_exists('errors', $result) and $result['errors']) {
                 throw new \Exception(is_array($result['errors']) ? implode("\n", $result['errors']) : $result['errors']);
             }
             return $result;
